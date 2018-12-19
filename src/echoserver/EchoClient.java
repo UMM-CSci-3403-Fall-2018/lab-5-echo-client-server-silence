7f9575b5087fb97cd91c57f96354e6b770439249
SirLich <lrkoehler77@gmail.com>
2018-12-18 21:25:50 -0600

    Implement system stream logic

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -7,6 +7,7 @@ public class EchoClient {
     public static final int portNumber = 6013;
 
     public static void main(String[] args) throws IOException {
+
         String server;
         // Use "127.0.0.1", i.e., localhost, if no server is specified.
         if (args.length == 0) {
@@ -21,12 +22,13 @@ public class EchoClient {
 
             // Get the input stream so we can read from that socket
             InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            OutputStream output = socket.getOutputStream();
 
             // Print all the input we receive from the server
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+            int tracker;
+            while ((tracker = System.in.read()) != -1) {
+                output.write(tracker);
+                System.out.write(input.read());
             }
 
             // Close the socket when we're done reading from it
